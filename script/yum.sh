dde28438217e24e1b6e040e3f0fdfd565a98d351
David Froger <david.froger@inria.fr>
2015-08-24 14:33:58 +0200

    Install git

--- script/yum.sh
+++ script/yum.sh
@@ -3,3 +3,6 @@ sudo yum -y  install \
   vim-enhanced.x86_64 \
   pkgconfig.x86_64 \
   screen.x86_64
+
+sudo rpm -Uvh http://repo.webtatic.com/yum/centos/5/latest.rpm
+sudo yum install -y --enablerepo=webtatic git-all
